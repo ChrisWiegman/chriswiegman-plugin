7f8946add728f9685564f41348a0aaf20bf29f71
Chris Wiegman <chris.wiegman@wpengine.com>
2021-07-18 19:59:32 -0400

    Version bump

--- plugin/chriswiegman-plugin.php
+++ plugin/chriswiegman-plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: ChrisWiegman.com Functionality
  * Plugin URI: https://chriswiegman.com
  * Description: Added functionality for ChrisWiegman.com
- * Version: 1.0.0
+ * Version: 1.0.1
  * Text Domain: chriswiegman-plugin
  * Domain Path: /languages
  * Author: Chris Wiegman
