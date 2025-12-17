429acf11c42d11800ec0e413f4fc04c46ba54b24
Chris Wiegman <chris@chriswiegman.com>
2025-12-17 07:12:15 -0600

    Disable Jetpack AI

--- chriswiegman-plugin.php
+++ chriswiegman-plugin.php
@@ -28,6 +28,9 @@ function cw_plugin_plugin_loader() {
 	add_action( 'wp_head', 'cw_plugin_add_mastodon_ownership' );
 	add_filter( 'the_content_feed', 'cw_plugin_add_featured_image_to_feed' );
 
+	// Disable Jetpack AI.
+	add_filter( 'jetpack_ai_enabled', '__return_false' );
+
 	// Remove WordPress' RUM scripts.
 	remove_action( 'wp_footer', 'wpcomsh_footer_rum_js' );
 	remove_action( 'admin_footer', 'wpcomsh_footer_rum_js' );
